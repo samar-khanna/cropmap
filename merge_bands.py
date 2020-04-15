22fa98793edfd52d768a8543fc90c1a74fddf0c1
Bhargava Manja <bmanja@protonmail.com>
2020-04-14 23:31:07 -0400

    added mosaic code to merge_bands

--- merge_bands.py
+++ merge_bands.py
@@ -1,11 +1,10 @@
-from concurrent.futures import ProcessPoolExecutor
-from glob import glob
 import os
 from os.path import join
 from pprint import pprint #TODO delete this
 import sys
 
 import rasterio
+from rasterio.merge import merge
 
 SR_BANDS = map(lambda n: 'SRB{}'.format(n), [1,2,3,4,5,6,7])
 BT_BANDS = map(lambda n: 'BTB{}'.format(n), [10,11])
@@ -22,11 +21,11 @@ def get_band_files(data_root, folders):
     return {f: get_folder_bands(f) for f in folders}
 
 
-def merge_bands(data_root, entityId, band_files):
-    # TODO clean up
+def merge_bands(data_root, entity_id, band_files):
+    # TODO clean up on failure
     dataset_objs = list(map(lambda f: rasterio.open(f), band_files))
     raster_objs = map(lambda d: d.read(1), dataset_objs)
-    comp_fname = join(data_root, entityId+'_'+'composite.tif')
+    comp_fname = join(data_root, entity_id+'_'+'composite.tif')
     composite = rasterio.open(
         comp_fname,
         'w',
@@ -43,15 +42,33 @@ def merge_bands(data_root, entityId, band_files):
         d.close()
 
     composite.close()
+    return comp_fname
 
 
 def main(data_root):
+    # TODO clean up on failure
     folders = os.listdir(data_root)
     folder_files = get_band_files(data_root, folders)
     #pprint({k: list(m) for k,m in folder_files.items()})
-    packed = list(folder_files.items())
-    for entityId, band_files in folder_files.items():
-        merge_bands(data_root, entityId, band_files)
+    composite_paths = [merge_bands(data_root, e, b) for e,b in folder_files.items()]
+    composites = [rasterio.open(c) for c in composite_paths]
+    mosaic, out_trans = merge(composites)
+    with rasterio.open(
+            os.path.join(data_root, 'mosaic.tif'), 
+            'w', 
+            driver='GTiff',
+            height=composites[0].height,
+            width=composites[0].width,
+            count=composites[0].count,
+            dtype=composites[0].dtypes[0],
+            crs=composites[0].crs,
+            nodata=composites[0].nodata,
+            transform=out_trans) as fp:
+        fp.write(mosaic)
+
+    composites[0].close()
+    composites[1].close()
+
 
 if __name__ == '__main__':
     data_root = sys.argv[1]

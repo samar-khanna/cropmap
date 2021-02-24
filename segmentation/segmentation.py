6ae19a282cbb97529b7203ec406e1da21c2aed22
Samar Khanna <samar.khanna@gmail.com>
2021-02-23 22:02:52 -0500

    Added RUNet model

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 
 from models.fcn import FCN
 from models.unet import UNet
+from models.runet import RUNet
 from models.m2unet import M2UNet
 from models.block_unet import BlockUNet
 from models.simple_net import SimpleNet
@@ -19,15 +20,17 @@ from data_loaders.time_series_loader import TimeSeriesDataset
 MODELS = {
     "fcn": FCN,
     "unet": UNet,
+    "runet": RUNet,
     "m2unet": M2UNet,
     "blockunet": BlockUNet,
-    "simplenet": SimpleNet
+    "simplenet": SimpleNet,
 }
 
 
 LOADERS = {
     "fcn": ImageDataset,
     "unet": ImageDataset,
+    "runet": TimeSeriesDataset,
     "m2unet": TimeSeriesDataset,
     "blockunet": TimeSeriesDataset,
     "simplenet": TimeSeriesDataset

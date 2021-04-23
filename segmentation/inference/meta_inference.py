39ef50bd36927633033999024570426d63432daf
Samar Khanna <samar.khanna@gmail.com>
2021-04-23 06:43:32 -0400

    Added correct slice operation for backprop

--- segmentation/inference/meta_inference.py
+++ segmentation/inference/meta_inference.py
@@ -248,15 +248,18 @@ class MetaInferenceAgent(InferenceAgent):
                     copy_model.train()
                     avg_loss = MeanMetric()
                     for rep in range(self.reps_per_shot):
-                        # Shift to correct device
-                        input_shots, labels = \
-                            self.dataset.shift_sample_to_device((input_shots, labels), self.device)
 
                         batch_loss = 0.
                         batch_limit = shots if self.shot_batch_limit is None else self.shot_batch_limit
                         for b in range(0, shots, batch_limit):
-                            x_ts = [x[b:b+batch_limit] for x in input_shots]
-                            ys = labels[b:b+batch_limit]
+                            if self.shot_batch_limit is None:
+                                x_ts, y_s = input_shots, labels
+                            else:
+                                x_ts = [x[b:b+batch_limit].clone() for x in input_shots]
+                                ys = labels[b:b+batch_limit].clone()
+
+                            # Shift to correct device
+                            x_ts, ys = self.dataset.shift_sample_to_device((x_ts, ys), self.device)
 
                             # x_ts, ys = self.dataset.shift_sample_to_device((x_ts, ys), self.device)
 

477e989dbc09b46752d3dce7ec4a2853bea18006
Samar Khanna <samar.khanna@gmail.com>
2021-04-26 13:34:24 -0400

    Added new Transformer to segmentation file

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -2,10 +2,11 @@ from models.fcn import FCN
 from models.unet import UNet
 from models.runet import RUNet
 from models.m2unet import M2UNet
+from models.dumb_net import DumbNet
 from models.block_unet import BlockUNet
 from models.simple_net import SimpleNet
-from models.dumb_net import DumbNet
 from models.transformer import Transformer
+from models.transformer_feature_extract import TransformerFeatureExtract
 
 from data_loaders.image_loader import ImageDataset
 from data_loaders.time_series_loader import TimeSeriesDataset
@@ -19,7 +20,8 @@ MODELS = {
     "blockunet": BlockUNet,
     "simplenet": SimpleNet,
     "dumbnet": DumbNet,
-    "transformer": Transformer
+    "transformer": Transformer,
+    "transformer_feature_extract": TransformerFeatureExtract,
 }
 
 
@@ -31,5 +33,6 @@ LOADERS = {
     "blockunet": TimeSeriesDataset,
     "simplenet": TimeSeriesDataset,
     "dumbnet": TimeSeriesDataset,
-    "transformer": TimeSeriesDataset
+    "transformer": TimeSeriesDataset,
+    "transformer_feature_extract": TransformerFeatureExtract,
 }

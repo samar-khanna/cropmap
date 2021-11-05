933879cff93e0880e2d18d37ec87cdef823d56b3
Samar Khanna <samar.khanna@gmail.com>
2021-11-04 21:46:39 -0700

    Plot by longitude, save results to avoid recompute

--- fall_21_prototyping/plot_data.py
+++ fall_21_prototyping/plot_data.py
@@ -4,6 +4,7 @@ import pickle
 import argparse
 import numpy as np
 from scipy.spatial.distance import cdist
+from sklearn.metrics import pairwise_distances
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 from matplotlib.colors import ListedColormap
@@ -19,7 +20,7 @@ def mercator(coords, w, h):
     return np.stack((y, x))
 
 
-def get_transformer_loss_feats(transformer, X, y, batch_size=1024, get_feats=False):
+def proc_transformer_loss_feats(transformer, X, y, batch_size=1024, get_feats=False):
     transformer.eval()
 
     XT = torch.from_numpy(X.reshape(X.shape[0], -1).astype(np.float32))
@@ -49,9 +50,61 @@ def get_transformer_loss_feats(transformer, X, y, batch_size=1024, get_feats=Fal
             batch_losses = loss_fn(pred, by)
             losses = torch.cat((losses, batch_losses))
 
+            if i % 10 == 0:
+                print(f"Loss at iter {i}: {round(batch_losses.mean().item(), 4)}")
+
     if get_feats:
         return losses.numpy(), feats.numpy()  # (N,), (N, d)
     return losses.numpy()
+
+
+def proc_nearest_feats(feats, target_inds, source_inds, k=100):
+    target_feats = feats[target_inds]  # (Nt, d)
+    source_feats = feats[source_inds]  # (Ns, d)
+
+    # dist = cdist(source_feats, target_feats)  # (Ns, Nt)
+    # nearest = np.argmax(-dist, axis=-1)  # (Ns,)
+    # source_anchors = anchors[nearest]  # (Ns, )
+
+    dist = cdist(target_feats, source_feats)  # (Nt, Ns)
+    # nearest = np.argmax(-dist, axis=-1)  # (Nt,)
+    nearest = np.argsort(dist, axis=-1)[:, :k]  # (Nt, k)
+
+    return nearest
+
+
+def get_transformer_loss_feats(checkpoint_dir, X, y, target_inds, source_inds, get_feats=True):
+    from transformer import Transformer
+    transformer = Transformer(len(interest_classes), 9, 2)
+    sd = torch.load(os.path.join(checkpoint_dir, 'checkpoint.bin'), map_location='cpu')
+    transformer.load_state_dict(sd)
+
+    feats_file = os.path.join(checkpoint_dir, 'features.pkl')
+    losses_file = os.path.join(checkpoint_dir, 'losses.pkl')
+    nearest_feats_file = os.path.join(checkpoint_dir, 'nearest_features.pkl')
+    if os.path.isfile(feats_file) and os.path.isfile(losses_file):
+        print(f"Loading features from {feats_file}, losses from {losses_file}")
+        with open(losses_file, 'rb') as f:
+            losses = pickle.load(f)
+        with open(feats_file, 'rb') as f:
+            feats = pickle.load(f)
+    else:
+        losses, feats = proc_transformer_loss_feats(transformer, X, y, get_feats=get_feats)
+        with open(losses_file, 'wb') as f:
+            pickle.dump(losses, f)
+        with open(feats_file, 'wb') as f:
+            pickle.dump(feats, f)
+
+    if os.path.isfile(nearest_feats_file):
+        print(f"Loading nearest neighbour indices from {nearest_feats_file}")
+        with open(nearest_feats_file, 'rb') as f:
+            nearest_feats = pickle.load(f)
+    else:
+        nearest_feats = proc_nearest_feats(feats, target_inds, source_inds,)
+        with open(nearest_feats_file, 'wb') as f:
+            pickle.dump(nearest_feats, f)
+
+    return transformer, losses, feats, nearest_feats
 
 
 def plot_transformer_loss(losses, coords,):
@@ -61,7 +114,7 @@ def plot_transformer_loss(losses, coords,):
     cmap = get_cmap('viridis', 100)
     colors = cmap(losses)
 
-    merc = mercator(coords, 100, 50)
+    merc = mercator(coords, 200, 100)
     pix = merc.T  # (N, 2)
     plot = plt.scatter(pix[:, 1], pix[:, 0], c=losses, cmap=cmap)
 
@@ -69,11 +122,8 @@ def plot_transformer_loss(losses, coords,):
     plt.show()
 
 
-def plot_transformer_feat(feats, coords, target_inds, source_inds):
-    pix = mercator(coords, 100, 50).T  # (N, 2)
-
-    target_feats = feats[target_inds]  # (Nt, d)
-    source_feats = feats[source_inds]  # (Ns, d)
+def plot_transformer_feat(nearest, coords, target_inds, source_inds, k=20):
+    pix = mercator(coords, 200, 100).T  # (N, 2)
 
     # anchors = np.arange(target_feats.shape[0])
     # anchors = anchors / np.max(anchors)
@@ -84,21 +134,22 @@ def plot_transformer_feat(feats, coords, target_inds, source_inds):
     # nearest = np.argmax(-dist, axis=-1)  # (Ns,)
     # source_anchors = anchors[nearest]  # (Ns, )
 
-    source_anchors = np.arange(source_feats.shape[0])
-    anchors = source_anchors / np.max(source_anchors)
-    cmap = get_cmap('viridis', len(source_anchors))
+    source_anchors = np.arange(source_inds.shape[0])
+    source_anchors = source_anchors / np.max(source_anchors)
+    cmap = get_cmap('RdYlGn', len(source_anchors))
+    # cmap = get_cmap('viridis', len(source_anchors))
 
-    dist = cdist(target_feats, source_feats)  # (Nt, Ns)
-    nearest = np.argmax(-dist, axis=-1)  # (Nt,)
-    target_anchors = source_anchors[nearest]
+    target_anchors = np.mean(source_anchors[nearest[:, :k]], axis=-1)  # (Nt,)
 
     source_pix = pix[source_inds]
-    plt.scatter(source_pix[:, 1], source_pix[:, 0], c=source_anchors, cmap=cmap)
+    plt.scatter(source_pix[:, 1], source_pix[:, 0], c=source_anchors, cmap=cmap, s=1, alpha=1)
 
     target_pix = pix[target_inds]
-    plt.scatter(target_pix[:, 1], target_pix[:, 0], c=target_anchors, cmap=cmap)
+    plt.scatter(target_pix[:, 1], target_pix[:, 0], c=target_anchors, cmap=cmap, s=1, alpha=1)
 
     plt.show()
+
+    return nearest
 
 
 def load_data_from_pickle(path_dir):
@@ -113,32 +164,22 @@ def load_data_from_pickle(path_dir):
 
 def passed_args():
     parser = argparse.ArgumentParser(description="Plot stuff")
-    parser.add_argument('--data_path', type=str, default='../data_grid', help='Path to data')
-    parser.add_argument('--checkpoint', type=str, help='Path to checkpoint')
+    parser.add_argument('--data_path', type=str, default='../data_grid/grid_10000', help='Path to data')
+    parser.add_argument('--checkpoint', type=str, help='Path to checkpoint dir')
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = passed_args()
 
-    from transformer import Transformer
-    transformer = Transformer(len(interest_classes), 9, 2)
-    sd = torch.load(args.checkpoint, map_location='cpu')
-    transformer.load_state_dict(sd)
-
     regions = sorted([f for f in os.listdir(args.data_path) if f.startswith('usa')])
-    source_regions = [regions[1], regions[2], regions[3]]
-    target_regions = [regions[0]]
+    source_regions = [regions[0], regions[1], regions[2]]
+    target_regions = [regions[3]]
 
     X, Y, coords = [], [], []
     last_ind, source_inds, target_inds = 0, [], []
     for r in regions:
         x, y, coord = load_data_from_pickle(os.path.join(args.data_path, r))
-
-        sorted_coord = np.lexsort((coord[:, 0], coord[:, 1]))  # (Sort by lon, then by lat)
-        x = x[sorted_coord]
-        y = y[sorted_coord]
-        coord = coord[sorted_coord]
 
         inds = list(range(last_ind, last_ind + x.shape[0]))
         if r in source_regions:
@@ -151,14 +192,35 @@ if __name__ == "__main__":
         Y.append(y)
         coords.append(coord)
 
+        print(f"Region {r} shape: {x.shape}")
+
     X = np.concatenate(X, axis=0)  # (N, c, t)
     Y = np.concatenate(Y, axis=0)  # (N,)
     coords = np.concatenate(coords, axis=0)  # (N, 2)  fmt: (lon, lat)
+    orig_source_inds = np.array(source_inds)  # (Ns,)
+    target_inds = np.array(target_inds)  # (Nt,)
+    print(X.shape)
 
-    losses, feats = get_transformer_loss_feats(transformer, X, Y, get_feats=True)
-    plot_transformer_feat(feats, coords, target_inds, source_inds)
+    # Sort for pretty display
+    source_coords = coords[orig_source_inds]
+    sorted_coords = np.lexsort((source_coords[:, 1], source_coords[:, 0]))  # (Sort by lat, then by lon)
+    source_inds = orig_source_inds[sorted_coords]
+    # X[source_inds] = X[source_inds][sorted_coords]
+    # Y[source_inds] = Y[source_inds][sorted_coords]
+    # coords[source_inds] = coords[source_inds][sorted_coords]
 
+    transformer, losses, feats, nearest = get_transformer_loss_feats(
+        args.checkpoint, X, Y, target_inds, source_inds, get_feats=True
+    )
 
+    # lat_lon = np.lexsort((source_coords[:, 0], source_coords[:, 1]))
+    # lat_lon_inds = orig_source_inds[lat_lon]
+    # map = np.lexsort((coords[lat_lon_inds][:, 1], coords[lat_lon_inds][:, 0]))
+    # assert np.all(lat_lon_inds[map] == source_inds)
+    # print(map)
+    #
+    # # feats[lat_lon_inds] = feats[source_inds]
+    # nearest = map[nearest]
+    plot_transformer_feat(nearest, coords, target_inds, source_inds, k=1)
 
-    pass
 

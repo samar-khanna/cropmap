1063e48aee4b3d51a6dd126d4dc3ae68f6552234
Samar Khanna <samar.khanna@gmail.com>
2021-11-09 00:21:37 -0800

    Accuracies (avg and weighted)

---
+++ fall_21_prototyping/util.py
@@ -0,0 +1,21 @@
+import numpy as np
+from collections import Counter
+from scipy.special import softmax
+
+
+def get_accuracies(class_seen, class_correct):
+    total_seen = sum(class_seen.values())
+    accuracy = sum(class_correct.values()) / total_seen
+
+    avg_class_acc = 0.
+    w_class_acc = 0.
+
+    # python order of values guaranteed
+    class_probs = softmax(class_seen.values())
+    for i, (c, c_seen) in enumerate(class_seen.items()):
+        avg_class_acc += (1/len(class_seen)) * class_correct[c] / c_seen
+        w_class_acc += class_probs * class_correct[c] / c_seen
+
+    return {'accuracy': accuracy,
+            'average class accuracy': avg_class_acc,
+            'weighted class accuracy': w_class_acc}

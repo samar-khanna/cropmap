1624bc573a1d353d61049dcea2956b97d6fdfec5
BramSW <bramswallace@gmail.com>
2021-11-10 15:05:44 -0500

    plotting

--- fall_21_prototyping/launcher.sh
+++ fall_21_prototyping/launcher.sh
@@ -1,14 +1,35 @@
-# for region in washington
-for region in washington california us_south corn_belt new_york
+# for weight in -0.5 -0.1 0 0.1 0.5
+for weight in -0.5 0 0.5
 do
-    # for barycenter_method in EM_barycenter SG_barycenter soft_barycenter mean
-    for barycenter_method in mean
+  for clfstr in transformer_correlation_input
+  do
+    for regioni in {2..2}
     do
-        # for samples in 10000
-        for samples in 99999999999999
+      for dataprepstrs in climate coords climate+coords normalize+climate normalize+coords normalize+climate+coords
+      do
+        for wd in 0 1e-4
         do
-            echo "$region $barycenter_method $samples"
-            sbatch cpu.sub $region $barycenter_method $samples
+          sbatch --requeue grid_job.sub --clf-strs $clfstr --data-prep-strs $dataprepstrs --weight $weight --generalization-region usa_g${regioni} --weight-decay $wd
         done
-    done
+       done
+     done
+  done
+done
+
+
+for weight in 0
+do
+  for clfstr in transformer
+  do
+    for regioni in {2..2}
+    do
+      for dataprepstrs in normalize none
+      do
+        for wd in 0 1e-4
+        do
+          sbatch --requeue grid_job.sub --clf-strs $clfstr --data-prep-strs $dataprepstrs --weight $weight --generalization-region usa_g${regioni} --weight-decay $wd
+        done
+       done
+     done
+  done
 done
